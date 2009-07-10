91f70dc91d22e8c62a33779a1ff46bc4f81ad8a3
Stas SUSHKOV <stas@code.c7obs.net>
2009-07-10 15:11:45 +0300

    Added Service Locs

--- app/views/html/partials/menu.php
+++ app/views/html/partials/menu.php
@@ -1,5 +1,6 @@
 <ul class="menu">
 	<li><?php echo getLink("Realms", "realms/") ?></li>
 	<li><?php echo getLink("Institutions", "institutions/") ?></li>
+	<li><?php echo getLink("Service Locations", "service_locs/") ?></li>
 	<li><?php echo getLink("Logout", "users/logout") ?></li>
 </ul>
