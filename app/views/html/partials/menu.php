7b234d0dc83765399225db0817ba6aff5c45153c
Stas SUSHKOV <stas@code.c7obs.net>
2009-07-15 11:29:50 +0300

    Added realm usage

--- app/views/html/partials/menu.php
+++ app/views/html/partials/menu.php
@@ -3,5 +3,6 @@
 	<li><?php echo getLink("Institutions", "institutions/") ?></li>
 	<li><?php echo getLink("Service Locations", "service_locs/") ?></li>
 	<li><?php echo getLink("Realm Data", "realm_datas/") ?></li>
+	<li><?php echo getLink("Realm Usage", "realm_usages/") ?></li>
 	<li><?php echo getLink("Logout", "users/logout") ?></li>
 </ul>
