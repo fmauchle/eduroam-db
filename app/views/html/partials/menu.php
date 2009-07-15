ba78b4b43714623118197e9ae7c8b2377236962f
Stas SUSHKOV <stas@code.c7obs.net>
2009-07-15 10:56:41 +0300

    Added realm_data

--- app/views/html/partials/menu.php
+++ app/views/html/partials/menu.php
@@ -2,5 +2,6 @@
 	<li><?php echo getLink("Realms", "realms/") ?></li>
 	<li><?php echo getLink("Institutions", "institutions/") ?></li>
 	<li><?php echo getLink("Service Locations", "service_locs/") ?></li>
+	<li><?php echo getLink("Realm Data", "realm_datas/") ?></li>
 	<li><?php echo getLink("Logout", "users/logout") ?></li>
 </ul>
