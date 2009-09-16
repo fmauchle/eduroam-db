6e8b2e28eff9da257898212b1e0a83d0c8cf7b05
Stas SUSHKOV <stas@net.utcluj.ro>
2009-09-16 22:33:32 +0300

    Fixed a typo in realm xml

--- app/views/xml/realms/xml.php
+++ app/views/xml/realms/xml.php
@@ -3,8 +3,8 @@
 <?php foreach($realms as $r): ?>
 	<realm>
 		<country><?php echo $r->data['country'] ?></country>
-		<stype><?php echo $r->data['country'] ?></stype>
-		<org_name lang="en"><?php echo $r->data['country'] ?></org_name>
+		<stype><?php echo $r->data['stype'] ?></stype>
+		<org_name lang="en"><?php echo $r->data['org_name'] ?></org_name>
 		<address>
 			<street><?php echo $r->data['address_street'] ?></street>
 			<city><?php echo $r->data['address_city'] ?></city>
