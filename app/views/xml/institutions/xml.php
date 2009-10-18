232e8870dcca4a8b753456388b630d12169d4d92
Stas SUSHKOV <stas@net.utcluj.ro>
2009-10-18 15:58:54 +0300

    Fixed the wrong lang= atribute. (closes #5)

--- app/views/xml/institutions/xml.php
+++ app/views/xml/institutions/xml.php
@@ -16,7 +16,10 @@ foreach ($realms as $r):
 		<country><?php echo $r->data['country']?></country>
 		<type><?php echo $ins->data['type']?></type>
 		<inst_realm><?php echo $ins->data['inst_realm']?></inst_realm>
-		<org_name lang="<?php echo $r->data['country']?>"><?php echo $ins->data['org_name']?></org_name>
+		<org_name lang="en"><?php echo $ins->data['org_name']?></org_name>
+<?php if(strtolower($r->data['country']) != "en"): ?>
+		<org_name lang="<?php echo strtolower($r->data['country'])?>"><?php echo $ins->data['org_name']?></org_name>
+<?php endif; ?>
 		<address>
 			<street><?php echo $ins->data['address_street']?></street>
 			<city><?php echo $ins->data['address_city']?></city>
@@ -31,15 +34,24 @@ foreach ($realms as $r):
 			<email><?php echo $r->data['contact_email']?></email>
 			<phone><?php echo $r->data['contact_phone']?></phone>
 		</contact>
-		<info_URL lang="<?php echo $r->data['country']?>"><?php echo $ins->data['info_url']?></info_URL>
-		<policy_URL lang="<?php echo $r->data['country']?>"><?php echo $ins->data['policy_url']?></policy_URL>
+		<info_URL lang="en"><?php echo $ins->data['info_url']?></info_URL>
+<?php if(strtolower($r->data['country']) != "en"): ?>
+		<info_URL lang="<?php echo strtolower($r->data['country'])?>"><?php echo $ins->data['info_url']?></info_URL>
+<?php endif; ?>
+		<policy_URL lang="en"><?php echo $ins->data['policy_url']?></policy_URL>
+<?php if(strtolower($r->data['country']) != "en"): ?>
+		<policy_URL lang="<?php echo strtolower($r->data['country'])?>"><?php echo $ins->data['policy_url']?></policy_URL>
+<?php endif; ?>
 		<ts><?php echo $ins->data['ts']?></ts>
 <?php 
 if($ins->data['sl']): foreach($ins->data['sl'] as $s): ?>
 		<location>
 			<longitude><?php echo $s->data['longitude']?></longitude>
 			<latitude><?php echo $s->data['latitude']?></latitude>
-			<loc_name lang="<?php echo $r->data['country']?>"><?php echo $s->data['loc_name']?></loc_name>
+			<loc_name lang="en"><?php echo $s->data['loc_name']?></loc_name>
+<?php if(strtolower($r->data['country']) != "en"): ?>
+			<loc_name lang="<?php echo strtolower($r->data['country'])?>"><?php echo $s->data['loc_name']?></loc_name>
+<?php endif; ?>
 			<address>
 				<street><?php echo $s->data['address_street']?></street>
 				<city><?php echo $s->data['address_city']?></city>
@@ -52,7 +64,10 @@ if($ins->data['sl']): foreach($ins->data['sl'] as $s): ?>
 			<NAT><?php echo t_or_f($s->data['NAT'])?></NAT>
 			<AP_no><?php echo $s->data['AP_no']?></AP_no>
 			<wired><?php echo t_or_f($s->data['wired'])?></wired>
-			<info_URL lang="<?php echo $r->data['country']?>"><?php echo $s->data['info_URL']?></info_URL>
+			<info_URL lang="en"><?php echo $s->data['info_URL']?></info_URL>
+<?php if(strtolower($r->data['country']) != "en"): ?>
+			<info_URL lang="<?php echo strtolower($r->data['country'])?>"><?php echo $s->data['info_URL']?></info_URL>
+<?php endif; ?>
 		</location>
 <?php endforeach; endif; ?>
 	</institution>
--- app/views/xml/realms/xml.php
+++ app/views/xml/realms/xml.php
@@ -5,6 +5,9 @@
 		<country><?php echo $r->data['country'] ?></country>
 		<stype><?php echo $r->data['stype'] ?></stype>
 		<org_name lang="en"><?php echo $r->data['org_name'] ?></org_name>
+<?php if(strtolower($r->data['country']) != "en"): ?>
+		<org_name lang="<?php echo strtolower($r->data['country']) ?>"><?php echo $r->data['org_name'] ?></org_name>
+<?php endif; ?>
 		<address>
 			<street><?php echo $r->data['address_street'] ?></street>
 			<city><?php echo $r->data['address_city'] ?></city>
@@ -14,8 +17,14 @@
 			<email><?php echo $r->data['contact_email'] ?></email>
 			<phone><?php echo $r->data['contact_phone'] ?></phone>
 		</contact>
-		<info_URL lang="en"><?php echo $r->data['info_URL'] ?></info_URL>
-		<policy_URL lang="en"><?php echo $r->data['policy_URL'] ?></policy_URL>
+		<info_URL lang="en"><?php echo $ins->data['info_url']?></info_URL>
+<?php if(strtolower($r->data['country']) != "en"): ?>
+		<info_URL lang="<?php echo strtolower($r->data['country'])?>"><?php echo $ins->data['info_url']?></info_URL>
+<?php endif; ?>
+		<policy_URL lang="en"><?php echo $ins->data['policy_url']?></policy_URL>
+<?php if(strtolower($r->data['country']) != "en"): ?>
+		<policy_URL lang="<?php echo strtolower($r->data['country'])?>"><?php echo $ins->data['policy_url']?></policy_URL>
+<?php endif; ?>
 		<ts><?php echo $r->data['ts'] ?></ts>
 	</realm>
 <?php endforeach; ?>
